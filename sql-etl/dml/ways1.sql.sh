88c7bb55b021491e1cf886e012129ba48d9b011e
Allen Day <allenday@allenday.com>
2019-12-06 15:43:05 -0800

    no need to insert, define it as bq cli arg

--- sql-etl/dml/ways1.sql.sh
+++ sql-etl/dml/ways1.sql.sh
@@ -1,4 +1,7 @@
-SELECT w.id, w.version, w.username, w.changeset, w.visible, w.osm_timestamp, t.nodes, w.geometry, w.bounding_box, t.all_tags FROM (
+echo "
+SELECT w.id, w.version, w.username, w.changeset, w.visible, w.osm_timestamp, t.nodes, w.geometry, 
+  \`${PROJECT}.${DATASET}.get_extent\`(w.geometry) AS extent, t.all_tags FROM
+(
 
 --linestring
 SELECT
@@ -8,22 +11,10 @@ SELECT
   ways.changeset,
   ways.visible,
   TIMESTAMP_SECONDS(CAST(ways.osm_timestamp/1000 AS INT64)) AS osm_timestamp,
-  COALESCE(SAFE.ST_MAKEPOLYGON(ST_MAKELINE(ARRAY_AGG(nodes.geometry))), SAFE.ST_MAKELINE(ARRAY_AGG(nodes.geometry))) AS geometry,
-  STRUCT(
-    SAFE.ST_MAKELINE([
-      ST_GEOGPOINT(MIN(ST_X(nodes.geometry)),MAX(ST_Y(nodes.geometry))),
-      ST_GEOGPOINT(MIN(ST_X(nodes.geometry)),MIN(ST_Y(nodes.geometry))),
-      ST_GEOGPOINT(MAX(ST_X(nodes.geometry)),MIN(ST_Y(nodes.geometry))),
-      ST_GEOGPOINT(MAX(ST_X(nodes.geometry)),MAX(ST_Y(nodes.geometry)))
-    ]) AS geometry,
-    ST_GEOGPOINT(MIN(ST_X(nodes.geometry)),MAX(ST_Y(nodes.geometry))) AS top_left,
-    ST_GEOGPOINT(MIN(ST_X(nodes.geometry)),MIN(ST_Y(nodes.geometry))) AS bottom_left,
-    ST_GEOGPOINT(MAX(ST_X(nodes.geometry)),MIN(ST_Y(nodes.geometry))) AS bottom_right,
-    ST_GEOGPOINT(MAX(ST_X(nodes.geometry)),MAX(ST_Y(nodes.geometry))) AS top_right
-  ) AS bounding_box
+  COALESCE(SAFE.ST_MAKEPOLYGON(ST_MAKELINE(ARRAY_AGG(nodes.geometry))), SAFE.ST_MAKELINE(ARRAY_AGG(nodes.geometry))) AS geometry
 FROM
-  `osmbigqueryloader_dev.planet_ways0` AS ways JOIN UNNEST(nodes) AS waynode,
-  `openstreetmap-public-data-dev.osmbigqueryloader_dev.planet_nodes1` AS nodes
+  \`${PROJECT}.${DATASET}.${PREFIX}_ways0\` AS ways JOIN UNNEST(nodes) AS waynode,
+  \`${PROJECT}.${DATASET}.${PREFIX}_nodes1\` AS nodes
 WHERE waynode = nodes.id AND NOT EXISTS(SELECT 1 FROM UNNEST(ways.all_tags) WHERE key = 'area' AND value = 'yes')
 GROUP BY ways.id, ways.version, ways.username, ways.changeset, ways.visible, ways.osm_timestamp
 
@@ -37,24 +28,13 @@ SELECT
   ways.changeset,
   ways.visible,
   TIMESTAMP_SECONDS(CAST(ways.osm_timestamp/1000 AS INT64)) AS osm_timestamp,
-  COALESCE(SAFE.ST_MAKEPOLYGON(ST_MAKELINE(ARRAY_AGG(nodes.geometry))), SAFE.ST_MAKELINE(ARRAY_AGG(nodes.geometry))) AS geometry,
-  STRUCT(
-    SAFE.ST_MAKEPOLYGON(SAFE.ST_MAKELINE([
-      ST_GEOGPOINT(MIN(ST_X(nodes.geometry)),MAX(ST_Y(nodes.geometry))),
-      ST_GEOGPOINT(MIN(ST_X(nodes.geometry)),MIN(ST_Y(nodes.geometry))),
-      ST_GEOGPOINT(MAX(ST_X(nodes.geometry)),MIN(ST_Y(nodes.geometry))),
-      ST_GEOGPOINT(MAX(ST_X(nodes.geometry)),MAX(ST_Y(nodes.geometry)))
-    ])) AS geometry,
-    ST_GEOGPOINT(MIN(ST_X(nodes.geometry)),MAX(ST_Y(nodes.geometry))) AS top_left,
-    ST_GEOGPOINT(MIN(ST_X(nodes.geometry)),MIN(ST_Y(nodes.geometry))) AS bottom_left,
-    ST_GEOGPOINT(MAX(ST_X(nodes.geometry)),MIN(ST_Y(nodes.geometry))) AS bottom_right,
-    ST_GEOGPOINT(MAX(ST_X(nodes.geometry)),MAX(ST_Y(nodes.geometry))) AS top_right
-  ) AS bounding_box
+  COALESCE(SAFE.ST_MAKEPOLYGON(ST_MAKELINE(ARRAY_AGG(nodes.geometry))), SAFE.ST_MAKELINE(ARRAY_AGG(nodes.geometry))) AS geometry
 FROM
-  `osmbigqueryloader_dev.planet_ways0` AS ways JOIN UNNEST(nodes) AS waynode,
-  `openstreetmap-public-data-dev.osmbigqueryloader_dev.planet_nodes1` AS nodes
+  \`${PROJECT}.${DATASET}.${PREFIX}_ways0\` AS ways JOIN UNNEST(nodes) AS waynode,
+  \`${PROJECT}.${DATASET}.${PREFIX}_nodes1\` AS nodes
 WHERE waynode = nodes.id AND EXISTS(SELECT 1 FROM UNNEST(ways.all_tags) WHERE key = 'area' AND value = 'yes')
 GROUP BY ways.id, ways.version, ways.username, ways.changeset, ways.visible, ways.osm_timestamp
 )
-AS w, `openstreetmap-public-data-dev.osmbigqueryloader_dev.planet_ways0` AS t
+AS w, \`${PROJECT}.${DATASET}.${PREFIX}_ways0\` AS t
 WHERE w.id = t.id
+"
