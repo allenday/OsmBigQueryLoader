168f9bd3bd047187a31cfa45a0f5231a2225ae1e
Allen Day <allenday@allenday.com>
2019-12-10 16:39:20 -0800

    use node with latest changeset <= way changeset. node version not relevant

--- sql-etl/dml/ways1.sql.sh
+++ sql-etl/dml/ways1.sql.sh
@@ -1,9 +1,15 @@
 echo "
-SELECT ways.id,ways.version,SAFE.ST_MAKELINE(ARRAY_AGG(nodes.geometry)) AS geometry
+--shard $W
+SELECT 
+  ways.id AS id,
+  ways.version AS version,
+  SAFE.ST_MAKELINE(ARRAY_AGG(nodes.geometry)) AS geometry
 FROM \`${PROJECT}.${DATASET}.${PREFIX}_ways0\` AS ways JOIN UNNEST(ways.nodes) AS waynodes
-JOIN \`${PROJECT}.${DATASET}.${PREFIX}_nodes1\` AS nodes ON (waynodes = nodes.id AND ways.version = nodes.version)
+JOIN \`${PROJECT}.${DATASET}.${PREFIX}_nodes1\` AS nodes ON (waynodes = nodes.id)
 WHERE ARRAY_LENGTH(ways.nodes) >= ($W)*100 AND ARRAY_LENGTH(ways.nodes) < ($W+1)*100
-GROUP BY ways.id, ways.version 
+  AND ways.changeset >= nodes.changeset
+GROUP BY ways.id, ways.version, nodes.version
+HAVING nodes.version = MAX(nodes.version)
 "
 
 #HAVING COUNT(nodes.geometry) >= 1900 AND COUNT(nodes.geometry) < 2000
